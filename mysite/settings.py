7dae10af6b09b2bf1903281e7ae1fc32cac464ba
vagrant <vagrant@localhost.localdomain>
2019-03-19 06:24:37 +0000

    database connected

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+	'NAME': 'webdb',
+	'USER': 'root',
+	'PASSWORD': 'Umb.ndnv-100%',
+	'HOST': '129.168.2.105',
+	'PORT': '8084',
     }
 }
 
